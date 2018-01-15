6e25a4a27c3b8200880139c460f306956838dd80
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-01-15 17:04:16 +0200

    boilerplate copy

---
+++ inc/specific/wupdates.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Handle updates via the WUpdates.com service.
+ *
+ * Put here the code provided by WUpdates.com for your theme.
+ *
+ * Development notice: This file is synced from the variations directory! Do not edit in the `inc` directory!
+ *
+ * @package Boilerplate
+ * @since 1.0.0
+ */
+
---
+++ variations/boilerplate/ts/App.ts
@@ -0,0 +1,3 @@
+import { Boilerplate } from './Boilerplate';
+
+new Boilerplate();
