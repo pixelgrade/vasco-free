e5ac258ce7106735d5b2240421466b9f08ee2e5b
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-01-19 16:46:03 +0200

    widgets minor refactoring and partial front page widgets nesting logic - see #58

--- inc/widgets/class-PromoBoxWidget.php
+++ inc/widgets/class-PromoBoxWidget.php
@@ -238,6 +238,17 @@ if ( ! class_exists( 'Pixelgrade_PromoBoxWidget' ) ) :
 				 */
 				$attributes = apply_filters( 'pixelgrade_promo_box_widget_attributes', array(), $instance, $this->id_base );
 
+				/**
+				 * Fires before the widget markup, including the <section>.
+				 *
+				 * This is a dynamic action specific to each widget instance.
+				 *
+				 * @param array $args     Display arguments including 'before_title', 'after_title',
+				 *                        'before_widget', and 'after_widget'.
+				 * @param array $instance An array of the widget's settings.
+				 */
+				do_action( 'pixelgrade_widget_before_' . $this->id, $args, $instance );
+
 				/*
 				 * Start outputting the widget markup
 				 */
@@ -262,6 +273,7 @@ if ( ! class_exists( 'Pixelgrade_PromoBoxWidget' ) ) :
 				</div>
 
 				<?php
+
 				/**
 				 * Fires at the end of the widget.
 				 *
@@ -273,6 +285,17 @@ if ( ! class_exists( 'Pixelgrade_PromoBoxWidget' ) ) :
 				do_action( 'pixelgrade_promo_box_widget_end', $args, $instance, $this->id_base );
 
 				echo $args['after_widget'];
+
+				/**
+				 * Fires after the widget markup, including the closing </section>.
+				 *
+				 * This is a dynamic action specific to each widget instance.
+				 *
+				 * @param array $args     Display arguments including 'before_title', 'after_title',
+				 *                        'before_widget', and 'after_widget'.
+				 * @param array $instance An array of the widget's settings.
+				 */
+				do_action( 'pixelgrade_widget_after_' . $this->id, $args, $instance );
 			} else {
 				// Let the developers know that something is amiss
 				_doing_it_wrong( __METHOD__, sprintf( 'Couldn\'t find a template part to use for displaying widget posts in the %s widget!', $this->name ), null );
