566334130e086a50ed4f72ac7ea7d882c3b7f73d
Victor Panaite <victor.panaite@pixelgrade.com>
2018-02-05 16:27:27 +0200

    [Stamps System] General Styling #51 review fixes

--- assets/js/app.bundle.js
+++ assets/js/app.bundle.js
@@ -588,7 +588,7 @@ var Bobo = function (_BaseTheme) {
                 var $element = __WEBPACK_IMPORTED_MODULE_1_jquery___default()(element);
                 var $text = $element.find('.c-stamp__text').first();
                 circleType = new __WEBPACK_IMPORTED_MODULE_2_circletype___default.a($text[0]);
-                circleType.radius(88).dir(-1);
+                circleType.radius(87).dir(-1);
                 if ($element.parent().hasClass('blob-container')) {
                     $element.addClass('c-stamp--rotated');
                 }
