93e6b21396b110ef1cf0c9cb9e4d6ee56467adae
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-02-08 11:53:16 +0200

    added some more core functions

--- template-parts/content-wp-instagram-widget-entry.php
+++ template-parts/content-wp-instagram-widget-entry.php
@@ -1,8 +1,23 @@
+<?php
+/**
+ * Template part for displaying a single image in the WP Instagram Widget output.
+ *
+ * @link https://wordpress.org/plugins/wp-instagram-widget/
+ *
+ * @package Bobo
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
 <li class="<?php echo esc_attr( $liclass ); ?>">
 	<a href="<?php echo esc_url( $item['link'] ); ?>"
 	   target="<?php echo esc_attr( $target ); ?>"
 	   class="<?php echo esc_attr( $aclass ); ?>">
-		<img src="<?php echo esc_url( $item[$size] ) ?>"
+		<img src="<?php echo esc_url( $item[ $size ] ) ?>"
 		     title='<?php echo wp_strip_all_tags( esc_attr( $item['description'] ) ); ?>'
 		     alt='<?php echo wp_strip_all_tags( esc_attr( $item['description'] ) ); ?>'
 		     class="<?php echo esc_attr( $imgclass ) ?>"/>
