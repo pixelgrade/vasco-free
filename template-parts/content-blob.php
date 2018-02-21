b7b922117d3d4196d2e7459186aeb8da4ba2df91
Razvan <razwan.me@gmail.com>
2018-02-21 12:21:54 +0200

    add blobs dispersion option

--- template-parts/content-blob.php
+++ template-parts/content-blob.php
@@ -13,7 +13,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="blobs  blobs--thumbnail">
-	<div class="blob  blob--shape-1  blob--color-1"></div>
-	<div class="blob  blob--shape-2  blob--color-2"></div>
-	<div class="blob  blob--shape-3  blob--color-3"></div>
+	<div class="blob">
+		<div class="blob-rotation">
+			<div class="blob-dispersion  blob--shape-1  blob--color-1"></div>
+		</div>
+	</div>
+	<div class="blob">
+		<div class="blob-rotation">
+			<div class="blob-dispersion  blob--shape-2  blob--color-2"></div>
+		</div>
+	</div>
+	<div class="blob">
+		<div class="blob-rotation">
+			<div class="blob-dispersion  blob--shape-3  blob--color-3"></div>
+		</div>
+	</div>
 </div>
