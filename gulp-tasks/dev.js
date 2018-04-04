369699ffe9fe042429aa2ae972866493e36d23e0
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-04-04 15:50:03 +0300

    rewritten gulp tasks for V4 - Yey

--- gulp-tasks/dev.js
+++ gulp-tasks/dev.js
@@ -24,50 +24,60 @@ function logError( err, res ) {
 	log( c.red( '> ' ) + err.file.split( '/' )[err.file.split( '/' ).length - 1] + ' ' + c.underline( 'line ' + err.line ) + ': ' + err.message );
 }
 
-gulp.task( 'styles-main', 'Compiles main css files (ie. style.css editor-style.css)', function() {
-	let variation = config.theme_name;
+function stylesMain() {
+    let variation = config.theme_name;
 
-	if ( argv.variation !== undefined ) {
-		variation = argv.variation;
-	}
+    if ( argv.variation !== undefined ) {
+        variation = argv.variation;
+    }
 
-	return gulp.src( 'variations/' + variation + '/scss/*.scss' )
-	           .pipe( plugins.sourcemaps.init() )
-	           .pipe( plugins.sass().on( 'error', logError ) )
-	           .pipe( plugins.autoprefixer() )
-	           .pipe( plugins.sourcemaps.write( '.' ) )
-	           .pipe( gulp.dest( '.' ) );
-} );
+    return gulp.src( 'variations/' + variation + '/scss/*.scss' )
+        .pipe( plugins.sourcemaps.init() )
+        .pipe( plugins.sass().on( 'error', logError ) )
+        .pipe( plugins.autoprefixer() )
+        .pipe( plugins.sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( '.' ) );
+}
+stylesMain.description = 'Compiles main css files (ie. style.css editor-style.css)';
+gulp.task( 'styles-main', stylesMain );
 
-gulp.task( 'styles-rtl', 'Generate rtl.css file based on style.css', function() {
-	return gulp.src( 'style.css' )
-	           .pipe( plugins.rtlcss() )
-	           .pipe( plugins.rename( 'rtl.css' ) )
-	           .pipe( gulp.dest( '.' ) );
-} );
+function stylesRtl() {
+    return gulp.src( 'style.css' )
+        .pipe( plugins.rtlcss() )
+        .pipe( plugins.rename( 'rtl.css' ) )
+        .pipe( gulp.dest( '.' ) );
+}
+stylesRtl.description = 'Generate rtl.css file based on style.css';
+gulp.task( 'styles-rtl', stylesRtl );
 
-gulp.task( 'styles-process', function() {
-	return gulp.src( 'style.css' )
-	           .pipe( plugins.sourcemaps.init( {loadMaps: true} ) )
-	           // @todo some processing
-	           .pipe( plugins.sourcemaps.write( '.' ) )
-	           .pipe( gulp.dest( '.' ) );
-} );
+function stylesProcess() {
+    return gulp.src( 'style.css' )
+        .pipe( plugins.sourcemaps.init( {loadMaps: true} ) )
+        // @todo some processing
+        .pipe( plugins.sourcemaps.write( '.' ) )
+        .pipe( gulp.dest( '.' ) );
+}
+gulp.task( 'styles-process', stylesProcess );
 
-gulp.task( 'styles-components', 'Compiles Sass and uses autoprefixer', function() {
-	return gulp.src( ['components/**/*.scss','!components/docs/**/*', '!components/.*/**/*'] )
-	           .pipe( plugins.sass().on( 'error', logError ) )
-	           .pipe( plugins.autoprefixer() )
-	           .pipe( plugins.rename( function( path ) {
-		           path.dirname = path.dirname.replace( '/scss', '' );
-		           path.dirname += "/css";
-	           } ) )
-	           .pipe( gulp.dest( './components' ) );
-} );
 
-gulp.task( 'styles', 'Compile styles', function( cb ) {
-	plugins.sequence( 'typeline-config', 'typeline-phpconfig', 'styles-components', 'styles-main', 'styles-rtl', cb );
-} );
+function stylesComponents() {
+    return gulp.src( ['components/**/*.scss','!components/docs/**/*', '!components/.*/**/*'] )
+        .pipe( plugins.sass().on( 'error', logError ) )
+        .pipe( plugins.autoprefixer() )
+        .pipe( plugins.rename( function( path ) {
+            path.dirname = path.dirname.replace( '/scss', '' );
+            path.dirname += "/css";
+        } ) )
+        .pipe( gulp.dest( './components' ) );
+}
+stylesComponents.description = 'Compiles Sass and uses autoprefixer';
+gulp.task( 'styles-components', stylesComponents );
+
+function stylesSequence(cb) {
+    plugins.sequence( 'typeline-config', 'typeline-phpconfig', 'styles-components', 'styles-main', 'styles-rtl', cb );
+}
+stylesSequence.description = 'Compile styles';
+gulp.task( 'styles', stylesSequence );
 
 
 
@@ -86,29 +96,31 @@ var jsFiles = [
 	'./assets/js/main/wrapper-end.js'
 ];
 
-gulp.task( 'scripts', 'Concatenate all JS into main.js and wrap all code in a closure', function() {
-	return gulp.src( jsFiles )
-	           .pipe( plugins.concat( 'main.js' ) )
-	           .pipe( gulp.dest( './assets/js/' ) );
-} );
+function scriptsConcatenate() {
+    return gulp.src( jsFiles )
+        .pipe( plugins.concat( 'main.js' ) )
+        .pipe( gulp.dest( './assets/js/' ) );
+}
+scriptsConcatenate.description = 'Concatenate all JS into main.js and wrap all code in a closure';
+gulp.task( 'scripts', scriptsConcatenate );
 
 
 // -----------------------------------------------------------------------------
 // Variation specific/synced files
 // -----------------------------------------------------------------------------
+function syncVariationSpecificFiles() {
+    let variation = config.theme_name;
 
-gulp.task( 'sync-variation-specific-files', [], function() {
-	let variation = config.theme_name;
+    if ( argv.variation !== undefined ) {
+        variation = argv.variation;
+    }
 
-	if ( argv.variation !== undefined ) {
-		variation = argv.variation;
-	}
+    del.sync( ['./inc/specific'] );
 
-	del.sync( ['./inc/specific'] );
-
-	return gulp.src( './variations/' + variation + '/synced/**/*' )
-		.pipe( gulp.dest( '.' ) );
-} );
+    return gulp.src( './variations/' + variation + '/synced/**/*' )
+        .pipe( gulp.dest( '.' ) );
+}
+gulp.task( 'sync-variation-specific-files', syncVariationSpecificFiles );
 
 
 // -----------------------------------------------------------------------------
@@ -122,26 +134,29 @@ gulp.task( 'sync-variation-specific-files', [], function() {
 // bit of time to run and don't need to happen on every file change. If you want
 // to run those tasks more frequently, set up a new watch task here.
 // -----------------------------------------------------------------------------
+function watchAll() {
+    gulp.watch( [
+        'inc/integrations/typeline-config.json',
+        'inc/integrations/typeline-config-editor.json'
+    ], ['typeline-config', 'typeline-phpconfig'] );
+    // We exclude the docs directory since that is not a true component; also exclude . directories
+    gulp.watch( ['variations/**/*.scss', 'components/**/*.scss', 'assets/scss/**/*.scss', '!components/docs/**/*', '!components/.*/**/*'], ['styles'] );
+    gulp.watch( 'assets/js/**/*.js', ['scripts'] );
+}
+watchAll.description = 'Watch for changes to various files and process them';
+gulp.task( 'watch', gulp.series('compile', watchAll) );
 
-gulp.task( 'watch', 'Watch for changes to various files and process them', ['compile'], function() {
-	gulp.watch( [
-		'inc/integrations/typeline-config.json',
-		'inc/integrations/typeline-config-editor.json'
-	], ['typeline-config', 'typeline-phpconfig'] );
-	// We exclude the docs directory since that is not a true component; also exclude . directories
-	gulp.watch( ['variations/**/*.scss', 'components/**/*.scss', 'assets/scss/**/*.scss', '!components/docs/**/*', '!components/.*/**/*'], ['styles'] );
-	gulp.watch( 'assets/js/**/*.js', ['scripts'] );
-} );
+function watchVariation() {
+    let variation = config.theme_name;
 
-gulp.task( 'watch-variation', 'Watch for changes to the variation synced files and directories and sync them', ['sync-variation-specific-files'], function() {
-	let variation = config.theme_name;
+    if ( argv.variation !== undefined ) {
+        variation = argv.variation;
+    }
 
-	if ( argv.variation !== undefined ) {
-		variation = argv.variation;
-	}
-
-	gulp.watch( 'variations/' + variation + '/synced/**/*', ['sync-variation-specific-files'] );
-} );
+    gulp.watch( 'variations/' + variation + '/synced/**/*', ['sync-variation-specific-files'] );
+}
+watchVariation.description = 'Watch for changes to the variation synced files and directories and sync them';
+gulp.task( 'watch-variation', gulp.series('sync-variation-specific-files', watchVariation) );
 
 
 
@@ -159,19 +174,19 @@ gulp.task( 'watch-variation', 'Watch for changes to the variation synced files a
 //
 // Usage: gulp browser-sync-proxy --port 8080
 // -----------------------------------------------------------------------------
-
-gulp.task( 'browser-sync', false, function() {
-	bs( {
-		// Point this to your pre-existing server.
-		proxy: config.baseurl + (
-			u.env.port ? ':' + u.env.port : ''
-		),
-		files: ['*.php', 'style.css', 'assets/js/main.js'],
-		// This tells BrowserSync to auto-open a tab once it boots.
-		open: true
-	}, function( err, bs ) {
-		if ( err ) {
-			console.log( bs.options );
-		}
-	} );
-} );
+function browserSync() {
+    bs( {
+        // Point this to your pre-existing server.
+        proxy: config.baseurl + (
+            u.env.port ? ':' + u.env.port : ''
+        ),
+        files: ['*.php', 'style.css', 'assets/js/main.js'],
+        // This tells BrowserSync to auto-open a tab once it boots.
+        open: true
+    }, function( err, bs ) {
+        if ( err ) {
+            console.log( bs.options );
+        }
+    } );
+}
+gulp.task( 'browser-sync', browserSync );
