02a8ab3351f325cfdbf09b65cda67083da16d8fa
Razvan <razwan.me@gmail.com>
2019-01-18 18:15:16 +0200

    git subrepo clone https://github.com/pixelgrade/components.git components
    subrepo:
      subdir:   "components"
      merged:   "897e781a"
    upstream:
      origin:   "https://github.com/pixelgrade/components.git"
      branch:   "master"
      commit:   "897e781a"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

---
+++ components/woocommerce/inc/template-tags.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Custom template tags for this component.
+ *
+ * Eventually, some of the functionality here could be replaced by core features.
+ *
+ * @see         https://pixelgrade.com
+ * @author      Pixelgrade
+ * @package     Components/Woocommerce
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+function is_woo_archive() {
+	return is_shop() || is_product_taxonomy();
+}
