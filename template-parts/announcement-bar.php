abdc43bb4fef3e12b2d6cf76ac4ae64b74f5ac2b
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-06-28 11:35:01 +0300

    Minor Theme Sniffer issues

--- template-parts/announcement-bar.php
+++ template-parts/announcement-bar.php
@@ -10,13 +10,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-$text = pixelgrade_option( 'announcement_bar_text' );
-$link = pixelgrade_option( 'announcement_bar_link' );
+$bar_text = pixelgrade_option( 'announcement_bar_text' );
+$bar_link = pixelgrade_option( 'announcement_bar_link' );
 
 ?>
 <div class="c-announcement-bar c-announcement-bar--hidden">
-    <a class="c-announcement-bar__text" href="<?php echo esc_url( $link ); ?>" target="_blank">
-        <?php echo wp_kses_post( $text ); // WPCS: XSS OK. ?>
+    <a class="c-announcement-bar__text" href="<?php echo esc_url( $bar_link ); ?>" target="_blank">
+        <?php echo wp_kses_post( $bar_text ); ?>
     </a>
     <a href class="c-announcement-bar__close js-announcement-bar__close">
         <?php get_template_part( 'template-parts/svg/icon-close' ); ?>
