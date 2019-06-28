ad8d7d2f83a1be6ef0d70d774902364394a931e7
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-06-28 11:40:50 +0300

    Removed Pixelgrade Care recommendation

--- inc/extras.php
+++ inc/extras.php
@@ -428,26 +428,6 @@ function vasco_maybe_load_pro_features() {
 // We want to do this as early as possible. So the zero priority is as intended.
 add_action( 'after_setup_theme', 'vasco_maybe_load_pro_features', 0 );
 
-/**
- * Handle the WUpdates theme identification.
- *
- * @param array $ids
- *
- * @return array
- */
-function vasco_wupdates_add_id_wporg( $ids = array() ) {
-	// First get the theme directory name (unique)
-	$slug = basename( get_template_directory() );
-
-	// Now add the predefined details about this product
-	// Do not tamper with these please!!!
-	$ids[ $slug ] = array( 'name' => 'Vasco', 'slug' => 'vasco', 'id' => 'v7zV3', 'type' => 'theme_modular_wporg', 'digest' => '1620a9f29f78ed470fc4e29113b9e084', );
-
-	return $ids;
-}
-// The 5 priority is intentional to allow for pro to overwrite.
-add_filter( 'wupdates_gather_ids', 'vasco_wupdates_add_id_wporg', 5, 1 );
-
 function vasco_change_cart_menu_item_location( $location ) {
 	$location = false;
 	return $location;
--- inc/lite/admin/admin.php
+++ inc/lite/admin/admin.php
@@ -5,16 +5,6 @@
  * @package Vasco
  */
 
-function vasco_lite_admin_setup() {
-
-	/**
-	 * Load and initialize Pixelgrade Care notice logic.
-	 */
-	require_once 'pixcare-notice/class-notice.php';
-	PixelgradeCare_Install_Notice::init();
-}
-add_action( 'after_setup_theme', 'vasco_lite_admin_setup' );
-
 function vasco_lite_admin_assets() {
 	wp_enqueue_style( 'vasco_lite_admin_style', get_template_directory_uri() . '/inc/lite/admin/css/admin.css', array(), '1.3.5', false );
 }
